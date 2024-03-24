72cbc86ab57d75ea3a818cb0e93a046863bfd056
Luis Enrique <perezcoronaluisenrique4@gmail.com>
2024-03-23 22:53:01 -0600

    Now detects video and not images.

--- detector.py
+++ detector.py
@@ -1,13 +1,36 @@
 import cv2
-face = cv2.CascadeClassifier(cv2.data)
 
-face_cassifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#Load the face classifier 
+face_cassifier = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
-image = cv2.imread('example2.jpg')
-img_color =cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#Start the video capture whith the camera
+cap = cv2.VideoCapture(0)
 
-detector = face_cassifier.detectMultiScale(img_color, scaleFactor=1.1, minNeighbors=5, minSize=(30,30) ,maxSize=(200,200))
+while True:
+    #Capture a frame of the video capture
+    ret, frame = cap.read()
 
-for (i, j, k, x) in detector:
-    cv2.rectangle(image,(i,j),(i+k,j+x), (0,255,0), 2)
-cv2.imshow('image', image)
+    if not ret:
+        print("Error al capturar el fotograma")
+        break
+
+    #Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #Detect faces in the frame
+    faces = face_cassifier.detectMultiScale(gray, 1.1, 4)
+
+    #Draw rectangles arround the faces that are detected
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (231, 74, 74), 3)
+
+    #Show the frame whit the detected faces
+    cv2.imshow('Video con Detección de Rostros', frame)
+
+    #Press "c" to exit 
+    if cv2.waitKey(1) & 0xFF == ord('c'):
+        break
+
+#Close all the windows
+cap.release()
+cv2.destroyAllWindows()
